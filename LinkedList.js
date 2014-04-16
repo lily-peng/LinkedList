2d0fef9de9132e9db0ba4781f85dd8c9d5b34dbe
lpeng32 <mintylilac@yahoo.com>
2014-04-16 16:46:52 -0400

    made LinkedList a prototype

--- LinkedList.js
+++ LinkedList.js
@@ -1,9 +1,3 @@
-var LinkedList = {
-    head: null,
-    tail: null,
-    length: 0
-};
-
 var makeNode = function(data) {
     return {
         data: data,
@@ -12,83 +6,92 @@ var makeNode = function(data) {
     };
 };
 
-LinkedList.append = function(data) {
-    var node = makeNode(data);
-    if (LinkedList.length === 0) {
-        LinkedList.head = node;
-        node.isHead = true;
-        node.next = null;
-    }
-    else {
-        LinkedList.tail.next = node;
-        node.isHead = false;
-        node.next = null;
-    }
-    LinkedList.tail = node;
-    LinkedList.length++;
-};
-
-LinkedList.remove = function() {
-    if (LinkedList.length === 0)
-        return;
-    else if (LinkedList.length == 1) {
-        LinkedList.head = null;
-        LinkedList.tail = null;
-    }
-    else {
-        var iter = LinkedList.head;
-        for (var i = 0; i < LinkedList.length; i++) {
-            if (iter.next != LinkedList.tail) {
+function LinkedList() {};
+LinkedList.prototype = {
+    head: null,
+    tail: null,
+    length : 0,
+    
+    append: function (data) {
+        var node = makeNode(data);
+        if (this.length === 0) {
+            this.head = node;
+            node.isHead = true;
+            node.next = null;
+        }
+        else {
+            this.tail.next = node;
+            node.isHead = false;
+            node.next = null;
+        }
+        this.tail = node;
+        this.length++;
+    },
+    
+    removeLast: function() {
+        if (this.length === 0)
+            return;
+        else if (this.length == 1) {
+            this.head = null;
+            this.tail = null;
+        }
+        else {
+            var iter = this.head;
+            for (var i = 0; i < this.length; i++) {
+                if (iter.next != this.tail) {
+                    iter = iter.next;
+                }
+                else {
+                    iter.next = null;
+                    this.tail = iter;
+                }
+            }
+            this.length--;
+        }
+    },
+    
+    findNtoLast: function(n) {
+        if (n === 0)
+            return this.tail;
+        else if (n > this.length){
+            return null;
+        }
+        else if (n == this.length-1) {
+            return this.head;
+        }
+        else {
+            var newNum = this.length - n;
+            var iter = this.head;
+            for (var i = 0; i < newNum; i++) {
+                if (i == newNum-1)
+                    return iter;
                 iter = iter.next;
             }
-            else {
-                iter.next = null;
-                LinkedList.tail = iter;
-            }
         }
-        LinkedList.length--;
     }
 };
 
-LinkedList.findNtoLast = function(n) {
-    if (n === 0)
-        return LinkedList.tail;
-    else if (n > LinkedList.length){
-        return null;
-    }
-    else if (n == LinkedList.length-1) {
-        return LinkedList.head;
-    }
-    else {
-        var newNum = LinkedList.length - n;
-        var iter = LinkedList.head;
-        for (var i = 0; i < newNum; i++) {
-            if (i == newNum-1)
-                return iter;
-            iter = iter.next;
-        }
-    }
-};
+
 
 
 // TESTS
+var ll = new LinkedList();
+ll.append("first");
+ll.append("second");
+ll.append("third");
+ll.append("fourth");
+ll.append("fifth");
+ll.append("sixth");
+ll.append("seventh");
+ll.removeLast();
 
-LinkedList.append("first");
-LinkedList.append("second");
-LinkedList.append("third");
-LinkedList.append("fourth");
-LinkedList.append("fifth");
-LinkedList.append("sixth");
-LinkedList.append("seventh");
-LinkedList.remove();
-
-console.log(LinkedList.head);
-console.log(LinkedList.tail);
-console.log(LinkedList.length);
-console.log("0: ", LinkedList.findNtoLast(0));
-console.log("1: ", LinkedList.findNtoLast(1));
-console.log("2: ", LinkedList.findNtoLast(2));
-console.log("3: ", LinkedList.findNtoLast(3));
-console.log("4: ", LinkedList.findNtoLast(4));
-console.log("5: ", LinkedList.findNtoLast(5));
-console.log("6: ", LinkedList.findNtoLast(6));
+console.log(ll.head);
+console.log(ll.tail);
+console.log(ll.length);
+console.log("0: ", ll.findNtoLast(0));
+console.log("1: ", ll.findNtoLast(1));
+console.log("2: ", ll.findNtoLast(2));
+console.log("3: ", ll.findNtoLast(3));
+console.log("4: ", ll.findNtoLast(4));
+console.log("5: ", ll.findNtoLast(5));
+console.log("6: ", ll.findNtoLast(6));
